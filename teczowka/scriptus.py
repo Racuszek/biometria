6afb0cd583b9550f4da46ee2e66042a8148afbdb
Racuszek <zofia.pienkowska@samorzad.agh.edu.pl>
2019-11-09 18:12:40 +0100

    Added comments to the code; test commit

--- teczowka/scriptus.py
+++ teczowka/scriptus.py
@@ -1,7 +1,9 @@
 import cv2 as cv
 
 img=cv.imread('iris.png')
-# cv.imshow('fuck', img)
+#1 Zamiana obrazu na obraz w skali szarości
 gray_img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-cv.imshow('gray fuck', gray_img)
+#2 Normalizacja histogramu
+
+
 cv.waitKey()
