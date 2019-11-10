fccede146ecc0f0d8c53a8b1d339c2bd7fe522b7
Racuszek <zofia.pienkowska@samorzad.agh.edu.pl>
2019-11-10 12:31:06 +0100

    Added a loop for all files in array; test commit

--- odcisk/scriptus.py
+++ odcisk/scriptus.py
@@ -3,32 +3,34 @@ import numpy as np
 # All images have been cropped in order to remove discolorations.
 fingerprints=['odcisk1.jpg', 'odcisk2.jpg', 'odcisk3.jpg', 'odcisk4.jpg']
 
-img=cv.imread('odcisk1.jpg')
-img_gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-img_norm=cv.equalizeHist(img_gray)
-blurred = cv.GaussianBlur(img_norm,(3,3),0)
-retval, img = cv.threshold(blurred, 100, 255, cv.THRESH_BINARY)
+for item in fingerprints:
+    img=cv.imread(item)
+    img_gray=cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img_norm=cv.equalizeHist(img_gray)
+    blurred = cv.GaussianBlur(img_norm,(3,3),0)
+    retval, img = cv.threshold(blurred, 100, 255, cv.THRESH_BINARY)
 
-size = np.size(img)
-skel = np.zeros(img.shape,np.uint8)
+    size = np.size(img)
+    skel = np.zeros(img.shape,np.uint8)
 
-element = cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
-flag=False
+    element = cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
+    flag=False
 
-while(not flag):
-    eroded = cv.erode(img, element)
-    temp = cv.dilate(eroded, element)
-    temp = cv.subtract(img, temp)
-    skel = cv.bitwise_or(skel, temp)
-    img = eroded.copy()
-    zeros = size - cv.countNonZero(img)
-    if zeros==size:
-        flag = True
+    while(not flag):
+        eroded = cv.erode(img, element)
+        temp = cv.dilate(eroded, element)
+        temp = cv.subtract(img, temp)
+        skel = cv.bitwise_or(skel, temp)
+        img = eroded.copy()
+        zeros = size - cv.countNonZero(img)
+        if zeros==size:
+            flag = True
+    # For some reasons the output image is negative, so it needs to be inverted.
+    imagem=cv.bitwise_not(skel)
 
-imagem=cv.bitwise_not(skel)
-cv.imshow("skel",imagem)
-cv.waitKey(0)
-cv.destroyAllWindows()
+    cv.imshow("skel",imagem)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 # cv.imshow('bin', threshold)
