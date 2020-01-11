f25aa314be7a330a8c2d2d26c90744281d3cd227
Racuszek <zofia.pienkowska@samorzad.agh.edu.pl>
2020-01-11 13:41:53 +0100

    Submitted final code for primitive motion detection.

--- ruch/scriptus.py
+++ ruch/scriptus.py
@@ -4,13 +4,9 @@ import numpy as np
 im1=cv.imread('background.jpg')
 im1=cv.cvtColor(im1, cv.COLOR_BGR2GRAY)
 im1=cv.GaussianBlur(im1,(11,11),0)
-# im1=cv.copyMakeBorder(im1, 1, 1, 1, 1, cv.BORDER_CONSTANT, (0, 0, 0))
-# cv.imshow('white', im1)
-# cv.waitKey()
 im2=cv.imread('hand.jpg')
 im2=cv.cvtColor(im2, cv.COLOR_BGR2GRAY)
 im2=cv.GaussianBlur(im2,(11,11),0)
-# im2=cv.copyMakeBorder(im2, 1, 1, 1, 1, cv.BORDER_CONSTANT, (0, 0, 0))
 height, width = im1.shape[:2]
 im3=im2-im1
 
@@ -26,7 +22,6 @@ for i in range(height):
 retval, img_fin = cv.threshold(im3, 230, 255, cv.THRESH_BINARY)
 element = cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
 
-# 
 img_fin=cv.dilate(img_fin, element, iterations=3)
 img_fin=cv.erode(img_fin, element, iterations=5)
 img_fin=cv.dilate(img_fin, element, iterations=5)
@@ -35,29 +30,13 @@ mask = np.zeros((height+2, width+2), np.uint8)
 cv.floodFill(im_flood, mask, (0,0), 255)
 hand_stencil = im_flood & img_fin
 
-# cv.imshow('stencil', hand_stencil)
-# cv.waitKey()
-
 stencil = np.ones(img_fin.shape).astype(img_fin.dtype)
 color=[255, 255, 255]
-
-# ------------ IT'S WORKING --------------
 
 contours, hier = cv.findContours(hand_stencil,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
 im2=backtorgb = cv.cvtColor(im2,cv.COLOR_GRAY2RGB)
 for i in range(len(contours)):
-	# print(cv.contourArea(contours[i]))
 	if cv.contourArea(contours[i])>1000:
-		im2=cv.drawContours(im2, contours, i, (0,0,250),1)
-# sheet = np.zeros(img_fin.shape).astype(img_fin.dtype)
-# for cnt in contours:
-# 	if cv.contourArea(cnt)>1000:
-# 		# print(cv.contourArea(cnt))
-# 		cv.fillPoly(stencil, [cnt], color)
-# 		result=cv.bitwise_and(im1, stencil)
-# 		cv.imshow('woo', stencil)
-# 		cv.waitKey()
-#         # print('saving...')
-#         # cv.imwrite('{}_cont.jpg'.format(facename[:-4]), result)
-cv.imshow('isit', im2)
-cv.waitKey()
+		im2=cv.drawContours(im2, contours, i, (0,0,255),2)
+
+cv.imwrite('result.jpg', im2)
