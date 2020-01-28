541ac92afcb1b248bcf0d53f7a558fedd685eb1f
Zofia Pieńkowska <zofia.pienkowska@comarch.com>
2020-01-28 13:53:40 +0100

    scriptus minor change

--- odciski2/scriptus.py
+++ odciski2/scriptus.py
@@ -1,16 +1,15 @@
-arr=[]
-# names=['kciuk_p_0', 'srodkowy_p_0', 'serdeczny_l_0', 'wskazujacy_p_0']
-names=['kciuk_p_0']
-for name in names:
-#     for i in range(1, 4):
-    for i in range(1, 2):
-        file=open(name+str(i)+'.txt')
-        all_lines=file.read().split('\n')
-        for line in all_lines:
-            if len(line)==0:
-                continue;
+def parse(filename):
+    with open(filename) as file:
+        lines=file.read().split('\n')
+    # print(lines)
+    for line in lines:
+        if line=='':
+            continue
+        else:
             if line[0].isalpha():
-                print(line)
+                name=line.split(' ')[0]
             else:
-                arr.append((line.split()[0], line.split()[1], line.split()[2]))
-print(arr)
+                print('* ')
+
+parse("kciuk_p_01.txt")
+print(rozwidlenia)
