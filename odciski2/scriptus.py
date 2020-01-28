9fce667728c552ac8f0429b17a03c8b07734205f
Racuszek <zofia.pienkowska@samorzad.agh.edu.pl>
2020-01-28 19:06:31 +0100

    Renamed files; implemented a parsing function for feature vector files" "

--- odciski2/scriptus.py
+++ odciski2/scriptus.py
@@ -1,4 +1,7 @@
 def parse(filename):
+    # Given a file with feature vectors, it returns a dictionary, where key is the minutia name
+    # and value is list of its feature vectors as tuples. We assume the file contains no double spaces.
+    dict_of_minutiae={}
     with open(filename) as file:
         lines=file.read().split('\n')
     # print(lines)
@@ -7,9 +10,18 @@ def parse(filename):
             continue
         else:
             if line[0].isalpha():
-                name=line.split(' ')[0]
+                name=line.split()[0]
+                print(name)
+                globals()[name]=[]
             else:
-                print('* ')
+                vector_as_list=[float(item) for item in line[1:].split(' ')]
+                vector_as_tuple=tuple(vector_as_list)
+                globals()[name].append(vector_as_tuple)
+    dict_of_minutiae['rozwidlenia']=rozwidlenia
+    dict_of_minutiae['zakonczenia']=zakonczenia
+    dict_of_minutiae['rdzen_delta']=rdzen_delta
 
-parse("kciuk_p_01.txt")
-print(rozwidlenia)
+    return dict_of_minutiae
+
+# print(parse('kciuk_p_01.txt'))
+
